890856ef78ed67b20b44c021d13daf6f1eb1ace7
Mariusz <mariopl@poczta.onet.eu>
2021-07-17 19:45:47 +0200

    fix formatting

--- client/client.js
+++ client/client.js
@@ -15,7 +15,8 @@ const subject = webSocket({
     next(closeEvent) { console.log(closeEvent); }
   }
 });
-const enterKeyups = fromEvent(message, "keyup").pipe(filter(event => event.key === "Enter"));
+const enterKeyups = fromEvent(message, "keyup")
+  .pipe(filter(event => event.key === "Enter"));
 const clicksInDisconnect = fromEvent(disconnect, "click");
 
 function clearMessageInput() {
