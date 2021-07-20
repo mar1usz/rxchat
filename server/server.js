8bb6f5aa5a3e862d42cbfd6c0b7f9d38178be57d
Mariusz <mariopl@poczta.onet.eu>
2021-07-20 22:41:42 +0200

    refactor server

--- server/server.js
+++ server/server.js
@@ -1,27 +1,51 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 });
 
-wss.on('connection', (ws, request) => {
+function initialize() {
+  wss.on('connection', (ws, request) => {
+    onConnection(request);
+
+    ws.on('message', data => {
+      onMessage(data);
+    });
+
+    ws.on('close', (code, reason) => {
+      onClose(code, reason);
+    });
+
+    ws.on('error', err => {
+      onClientError(err);
+    });
+  });
+
+  wss.on('error', err => {
+    onServerError(err);
+  });
+}
+
+function onConnection(request) {
   console.log(request.rawHeaders);
+}
 
-  ws.on('message', data => {
-    for (const client of wss.clients) {
-      if (client.readyState === WebSocket.OPEN)
-        client.send(data);
-    }
+function onMessage(data) {
+  for (const client of wss.clients) {
+    if (client.readyState === WebSocket.OPEN)
+      client.send(data);
+  }
 
-    console.log(data);
-  });
+  console.log(data);
+}
 
-  ws.on('close', (code, reason) => {
-    console.log({ code, reason });
-  });
+function onClose(code, reason) {
+  console.log({ code, reason });
+}
 
-  ws.on('error', err => {
-    console.error(err);
-  });
-});
-
-wss.on('error', err => {
+function onClientError(err) {
   console.error(err);
-});
+}
+
+function onServerError(err) {
+  console.error(err);
+}
+
+initialize();
