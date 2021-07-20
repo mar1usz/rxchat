355fe11be0e7702af33803f6c678a68a335e1545
Mariusz <mariopl@poczta.onet.eu>
2021-07-20 22:45:03 +0200

    fix formatting

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8081 });
 
 function initialize() {
-  wss.on('connection', (ws, request) => {
+  wss.on("connection", (ws, request) => {
     onConnection(request);
 
-    ws.on('message', data => {
+    ws.on("message", (data) => {
       onMessage(data);
     });
 
-    ws.on('close', (code, reason) => {
+    ws.on("close", (code, reason) => {
       onClose(code, reason);
     });
 
-    ws.on('error', err => {
+    ws.on("error", (err) => {
       onClientError(err);
     });
   });
 
-  wss.on('error', err => {
+  wss.on("error", (err) => {
     onServerError(err);
   });
 }
@@ -29,8 +29,9 @@ function onConnection(request) {
 
 function onMessage(data) {
   for (const client of wss.clients) {
-    if (client.readyState === WebSocket.OPEN)
+    if (client.readyState === WebSocket.OPEN) {
       client.send(data);
+    }
   }
 
   console.log(data);
